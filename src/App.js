70d085e0eea95a986398a040e73ca206661f62b1
Albert Chung <chung972@gmail.com>
2019-07-09 12:11:02 -0500

    adds child components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
 
+
+// CHILD COMPONENTS
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to React Firebase TODOs</h1>
+    </div>
+  );
+}
+
+function Dashboard() {
+  // try not to have more than one h1 on any given page
+  return (
+    <div>
+      <h2>Welcome to your Dashboard</h2>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h2>You need to be logged in to see this page</h2>
+      <button>Login with Google</button>
+    </div>
+  );
+}
+
+// PARENT COMPONENT
 class App extends Component {
   render() {
     return (
-      <h1>Welcome to React Firebase TODOs</h1>
+      Home()
     );
   };
 }
